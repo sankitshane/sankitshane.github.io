e1ba72afb967ac370ae5e6822fc0af36e12f7794
sankit acharya <sankit.acharya@gmail.com>
2017-03-04 07:54:02 +0530

    fec: parallax completed

--- editor/script.js
+++ editor/script.js
@@ -1,3 +1,20 @@
 $(window).scroll(function(){
-  console.log("yay working!")
+
+  var wScroll = $(this).scrollTop();
+
+  $('.dp').css({
+    'transform':'translate(0px, '+ wScroll/5.4 +'%)'
+  });
+
+  $('.para1').css({
+    'transform':'translate(0px, '+ wScroll /6.12 +'%)'
+  });
+
+  $('.para2').css({
+    'transform':'translate(0px, -'+ wScroll /65 +'%)'
+  });
+
+  $('.para3').css({
+    'transform':'translate(0px, -'+ wScroll /65 +'%)'
+  });
 });
