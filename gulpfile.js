3b9ce71dd80d000b2a78be4af32e13b82356fedb
sankit acharya <sankit.acharya@gmail.com>
2017-03-18 03:35:46 +0530

    fix: server gzip compression added

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,5 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
-var uglify_css = require('gulp-uglifycss');
 var sass = require('gulp-sass');
 var pug = require('gulp-pug');
 
@@ -16,15 +15,18 @@ gulp.task('script', function() {
   .pipe(gulp.dest('master'))
 });
 
-gulp.task('style', function() {
-  gulp.src('editor/*.css')
-  .pipe(uglify_css())
-  .pipe(gulp.dest('master/style'))
-});
-
 gulp.task('Csass',function(){
     gulp.src('editor/main.sass')
-    .pipe(sass())
+    .pipe(sass({outputStyle: 'compressed'}))
+    .pipe(gulp.dest('editor/css'));
+    gulp.src('editor/screen50.sass')
+    .pipe(sass({outputStyle: 'compressed'}))
+    .pipe(gulp.dest('editor/css'));
+    gulp.src('editor/screen70.sass')
+    .pipe(sass({outputStyle: 'compressed'}))
+    .pipe(gulp.dest('editor/css'));
+    gulp.src('editor/screen80.sass')
+    .pipe(sass({outputStyle: 'compressed'}))
     .pipe(gulp.dest('editor/css'));
 });
 
@@ -33,4 +35,4 @@ gulp.task('watch',function() {
     gulp.watch('editor/*.sass',['Csass'])
 });
 
-gulp.task('default',['script','Csass','style']);
+gulp.task('default',['script','Csass']);
